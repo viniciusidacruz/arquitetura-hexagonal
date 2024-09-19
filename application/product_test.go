654f19b641456deee4c3e8378cecf18142a76b21
vinicius <viniciusidacruz@hotmail.com>
2024-09-19 00:22:28 -0300

    feat: :sparkles: Implement others methods for product
    Implement others methods for product

--- application/product_test.go
+++ application/product_test.go
@@ -34,3 +34,31 @@ func TestProduct_Disable(t *testing.T) {
     err = product.Disable()
     require.Error(t, err)
 }
+
+func TestProduct_GetID(t *testing.T) {
+	product := application.Product{}
+    product.ID = "test_id"
+
+    require.Equal(t, product.GetID(), "test_id")
+}
+
+func TestProduct_GetName(t *testing.T) {
+	product := application.Product{}
+    product.Name = "test product"
+
+    require.Equal(t, product.GetName(), "test product")
+}
+
+func TestProduct_GetPrice(t *testing.T) {
+	product := application.Product{}
+    product.Price = 100.2
+
+    require.Equal(t, product.GetPrice(), 100.2)
+}
+
+func TestProduct_GetStatus(t *testing.T) {
+	product := application.Product{}
+    product.Status = application.ENABLED
+
+    require.Equal(t, product.GetStatus(), application.ENABLED)
+}
